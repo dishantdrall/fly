3d9bf98f8e3ee7972842a72308218c1208a51297
dishant <joons7334@gmail.com>
2020-12-07 20:56:07 +0530

    display chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var clog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +38,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    clog = new Log(250,200,100,PI/2);
 
+   chain=new Chain(bird.body,clog.body);
 }
 
 function draw(){
@@ -61,4 +66,8 @@ function draw(){
 
     bird.display();
     platform.display();
-}
+    clog.display();
+
+    chain.display();
+
+   }
